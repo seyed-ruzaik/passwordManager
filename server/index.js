16339b052190a13964628b07f74992de204820d8
seyed_ruzaik <ruzaik.2018439@iit.ac.lk>
2022-03-01 08:12:19 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-
+require('dotenv').config();
 const database = require("./models");
 app.use(express.json())
 
+app.get("/", (req, res)=>{
+    res.status(200).send("Server is running");
+})
 
 //Routes
 const userRoutes = require("./routes/Users.routes");
@@ -13,7 +16,7 @@ app.use("/user",userRoutes);
 app.use("/passwords", passwordRoutes);
 
 database.sequelize.sync().then(()=>{
-    app.listen(3001, ()=>{
-        console.log('Server is running on port 3001');
+    app.listen(process.env.DB_PORT,()=>{
+        console.log('Server is running on port', process.env.DB_PORT);
     })
 })
